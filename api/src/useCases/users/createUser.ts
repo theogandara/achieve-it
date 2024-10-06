edf74a5e8a24b8c8e142e422f4181ad255aa8db1
Théo Gândara <theogandara@gmail.com>
2024-10-07 00:34:52 +0100

    Add DailyTemplate creation on user creation

--- api/src/useCases/users/createUser.ts
+++ api/src/useCases/users/createUser.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import bcrypt from 'bcryptjs';
 import { User } from '../../app/User';
+import { DailyTemplate } from '../../app/DailyTemplate';
 
 export async function createUser(req: Request, res: Response) {
   try {
@@ -8,6 +9,15 @@ export async function createUser(req: Request, res: Response) {
     const salt = bcrypt.genSaltSync(10);
     const hashedPassword = bcrypt.hashSync(password, salt);
     const user = await User.create({ email, password: hashedPassword });
+
+    await DailyTemplate.create({
+      email,
+      items: [
+        {
+          name: 'Wake up at 5:59',
+        },
+      ],
+    });
 
     return res.status(201).json(user);
   } catch (error) {
