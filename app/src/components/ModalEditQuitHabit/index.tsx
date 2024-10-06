d43cfc8029495721fdfc4b4b528c67844bd8e5bc
Théo Gândara <theogandara@gmail.com>
2024-10-07 00:39:46 +0100

    Add ModalEditQuitHabit component with styles

---
+++ app/src/components/ModalEditQuitHabit/index.tsx
@@ -0,0 +1,172 @@
+import { Modal, TextInput, View } from 'react-native';
+import { Button } from '../Button/button';
+import { Close } from '../Icons/Close';
+import { Text } from '../Text';
+import * as S from './styles';
+import { useRef, useState } from 'react';
+import { Input } from '../Input';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { Habit } from '../../pages/QuitHabits/QuitHabits';
+
+type ModalEditQuitHabitProps = {
+  isVisible: boolean;
+  onClose: () => void;
+  habit: Habit | null;
+};
+
+export function ModalEditQuitHabit({
+  isVisible,
+  onClose,
+  habit,
+}: ModalEditQuitHabitProps) {
+  if (!isVisible) return null;
+
+  function handleClose() {
+    onClose();
+  }
+
+  const [customHabit, setCustomHabit] = useState(habit?.title || '');
+  const [date, setDate] = useState(new Date(habit?.lastTime || Date.now()));
+  const [emoji, setEmoji] = useState('💀');
+
+  function handleReset() {
+    const newDate = new Date();
+    setDate(newDate);
+  }
+
+  function handleCreateHabit() {
+    const habit = {
+      id: Math.random(),
+      title: customHabit,
+      lastTime: date.toISOString(),
+    };
+
+    console.log('create habit', habit);
+    onClose();
+  }
+
+  const inputTextRef = useRef<TextInput>(null);
+
+  return (
+    <Modal
+      visible={isVisible}
+      animationType="slide"
+      presentationStyle="pageSheet"
+      onRequestClose={onClose}
+    >
+      <S.ModalBody>
+        <S.CloseButton onPress={handleClose}>
+          <Close color="#fff" />
+        </S.CloseButton>
+
+        <S.Header>
+          <Text size={24} weight="600" color="#fff">
+            Edit
+          </Text>
+        </S.Header>
+
+        <View
+          style={{
+            marginTop: 24,
+          }}
+        >
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              gap: 16,
+            }}
+          >
+            <View style={{ flex: 1 }}>
+              <Input
+                placeholder="Custom habit"
+                value={customHabit}
+                onChangeText={setCustomHabit}
+              />
+            </View>
+
+            <S.HabitItem
+              style={{ padding: 16, marginTop: 8 }}
+              onPress={() => {
+                inputTextRef.current?.focus();
+              }}
+            >
+              <TextInput
+                ref={inputTextRef}
+                style={{
+                  fontSize: 16,
+                  color: '#fff',
+                  textAlign: 'center',
+                  width: 24,
+                }}
+                value={emoji}
+                onKeyPress={(e) => {
+                  if (e.nativeEvent.key === 'Backspace') {
+                    setEmoji('');
+                    return;
+                  }
+                  setEmoji(e.nativeEvent.key);
+                }}
+              />
+            </S.HabitItem>
+          </View>
+
+          <S.HabitItem style={{ marginTop: 12 }}>
+            <Text
+              size={22}
+              color="#fff"
+              weight="600"
+              style={{ marginLeft: 12 }}
+            >
+              {date.toLocaleString()}
+            </Text>
+          </S.HabitItem>
+
+          <DateTimePicker
+            value={date}
+            mode="date"
+            textColor="#fff"
+            display="spinner"
+            style={{
+              height: 150,
+            }}
+            onChange={(_, selectedDate) => {
+              if (selectedDate) {
+                setDate(selectedDate);
+              }
+            }}
+          />
+
+          <DateTimePicker
+            value={date}
+            mode="time"
+            textColor="#fff"
+            display="spinner"
+            style={{
+              height: 150,
+            }}
+            onChange={(_, selectedDate) => {
+              if (selectedDate) {
+                setDate(selectedDate);
+              }
+            }}
+          />
+        </View>
+      </S.ModalBody>
+
+      <S.Footer>
+        <S.FooterContainer>
+          <Button onPress={handleCreateHabit}>Update habit</Button>
+
+          <S.Button onPress={handleReset}>
+            <Text weight="600" color="#fff">
+              Reset
+            </Text>
+          </S.Button>
+        </S.FooterContainer>
+      </S.Footer>
+    </Modal>
+  );
+}
---
+++ app/src/components/ModalEditQuitHabit/styles.ts
@@ -0,0 +1,52 @@
+import styled from 'styled-components/native';
+
+export const CloseButton = styled.TouchableOpacity`
+  width: 32px;
+  height: 32px;
+  background-color: rgba(0, 0, 0, 0.5);
+  border-radius: 16px;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  right: 24px;
+  top: 24px;
+`;
+
+export const Header = styled.View`
+  max-width: 80%;
+  gap: 8px;
+`;
+
+export const HeaderText = styled.View``;
+
+export const ModalBody = styled.View`
+  background: #1c1c1e;
+  padding: 32px 24px 0px;
+  flex: 1;
+`;
+
+export const Footer = styled.View`
+  background: #1c1c1e;
+  min-height: 110px;
+  padding: 16px 24px;
+`;
+
+export const FooterContainer = styled.SafeAreaView``;
+
+export const HabitItem = styled.TouchableOpacity`
+  padding: 12px;
+  background: #2c2c2e;
+  border-radius: 4px;
+  margin-bottom: 8px;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Button = styled.TouchableOpacity`
+  justify-content: center;
+  align-items: center;
+  border-radius: 4px;
+  padding: 14px 24px;
+  width: 100%;
+  margin-top: 24px;
+`;
