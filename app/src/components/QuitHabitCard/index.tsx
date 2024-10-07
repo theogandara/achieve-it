cdb7b97535d6a2f06b42545c5f24b6ca99caef82
Théo Gândara <theogandara@gmail.com>
2024-10-07 01:37:43 +0100

    Refactor QuitHabitCard component to improve time calculation and display

--- app/src/components/QuitHabitCard/index.tsx
+++ app/src/components/QuitHabitCard/index.tsx
@@ -10,23 +10,54 @@ type QuitHabitCardProps = {
 
 export function QuitHabitCard({ habit, onPress }: QuitHabitCardProps) {
   function handleTimeUntilNow(date: string) {
-    const now = new Date().getTime();
-    const lastTime = new Date(date).getTime();
-    const diff = now - lastTime;
+    const now = new Date();
+    const lastTime = new Date(date);
 
-    const diffDays = Math.floor(diff / (1000 * 60 * 60 * 24));
-    const diffHours = Math.floor(
-      (diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    const diffMinutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-    const diffSeconds = Math.floor((diff % (1000 * 60)) / 1000);
+    let diffYears = now.getFullYear() - lastTime.getFullYear();
+    let diffMonths = now.getMonth() - lastTime.getMonth();
+    let diffDays = now.getDate() - lastTime.getDate();
+    let diffHours = now.getHours() - lastTime.getHours();
+    let diffMinutes = now.getMinutes() - lastTime.getMinutes();
+    let diffSeconds = now.getSeconds() - lastTime.getSeconds();
 
+    if (diffSeconds < 0) {
+      diffSeconds += 60;
+      diffMinutes--;
+    }
+
+    if (diffMinutes < 0) {
+      diffMinutes += 60;
+      diffHours--;
+    }
+
+    if (diffHours < 0) {
+      diffHours += 24;
+      diffDays--;
+    }
+
+    if (diffDays < 0) {
+      const daysInLastMonth = new Date(
+        now.getFullYear(),
+        now.getMonth(),
+        0
+      ).getDate();
+      diffDays += daysInLastMonth;
+      diffMonths--;
+    }
+
+    if (diffMonths < 0) {
+      diffMonths += 12;
+      diffYears--;
+    }
+
+    const diffYearsString = diffYears > 0 ? `${diffYears}y ` : '';
+    const diffMonthsString = diffMonths > 0 ? `${diffMonths}m ` : '';
     const diffDaysString = diffDays > 0 ? `${diffDays}d ` : '';
     const diffHoursString = diffHours > 0 ? `${diffHours}h ` : '';
     const diffMinutesString = diffMinutes > 0 ? `${diffMinutes}m ` : '';
     const diffSecondsString = diffSeconds > 0 ? `${diffSeconds}s ` : '';
 
-    return `${diffDaysString}${diffHoursString}${diffMinutesString}${diffSecondsString}`;
+    return `${diffYearsString}${diffMonthsString}${diffDaysString}${diffHoursString}${diffMinutesString}${diffSecondsString}`.trim();
   }
 
   const [timeUntilNow, setTimeUntilNow] = useState(
@@ -42,10 +73,10 @@ export function QuitHabitCard({ habit, onPress }: QuitHabitCardProps) {
       <S.ItemList>
         <S.HeaderCard>
           <Text weight="700" size={22} color="#fff">
-            💀
+            {habit.icon}
           </Text>
           <Text weight="700" size={22} color="#fff">
-            {habit.title}
+            {habit.name}
           </Text>
         </S.HeaderCard>
 
