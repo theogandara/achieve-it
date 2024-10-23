e8604060f8609727c8fe426b7ee50da4dfb484a9
Théo Gândara <theogandara@gmail.com>
2024-10-23 21:09:43 +0100

    Refactor main.tsx to import GlobalStyle component for global CSS styles

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { PrivateRoutes } from "./private-routes.tsx";
-import "./global.css";
+import { GlobalStyle } from "./styles/globalStyle.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <PrivateRoutes />
+    <GlobalStyle />
   </StrictMode>
 );
