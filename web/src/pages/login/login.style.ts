f0f4c277868610e9a98ac05387b137b099ef3acc
Théo Gândara <theogandara@gmail.com>
2024-10-23 21:10:24 +0100

    Refactor login page CSS to use styled-components for styling

---
+++ web/src/pages/login/login.style.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  width: 100%;
+  max-width: 430px;
+  margin: auto;
+  margin-top: 100px;
+  padding: 1rem;
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`;
