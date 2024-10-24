28177e35cc54630be285fc16676874c3ed7c89a9
Théo Gândara <theogandara@gmail.com>
2024-10-24 20:03:31 +0100

    Refactor Checkbox component to use styled-components for consistent styling

---
+++ web/src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,21 @@
+import CheckboxEmpty from "../icons/checkbox/empty";
+import CheckboxFull from "../icons/checkbox/full";
+import * as S from "./checkbox.styles";
+
+type CheckboxProps = {
+  id: string;
+  done: boolean;
+  name: string;
+  onChange: (id: string) => void;
+};
+
+export default function Checkbox({ id, done, onChange, name }: CheckboxProps) {
+  return (
+    <div>
+      <S.CardReportItem key={id} onClick={() => onChange(id)}>
+        {done ? <CheckboxFull /> : <CheckboxEmpty />}
+        <p>{name}</p>
+      </S.CardReportItem>
+    </div>
+  );
+}
