b77332ea23b4ea0bdc6fa304e3f5f10a8e4b5b94
Théo Gândara <theogandara@gmail.com>
2024-10-24 20:13:56 +0100

    Refactor global font family to use "Poppins" instead of "Roboto"

--- web/src/styles/globalStyle.ts
+++ web/src/styles/globalStyle.ts
@@ -21,7 +21,7 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    font-family: "Roboto", sans-serif;
+    font-family: "Poppins", sans-serif;
     margin: 0;
     padding: 0;
     box-sizing: border-box;
