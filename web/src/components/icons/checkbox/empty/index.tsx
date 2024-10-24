6bc3259d42ed5aa8cc30d7dec2acbe3e52dc3d07
Théo Gândara <theogandara@gmail.com>
2024-10-24 20:03:40 +0100

    Add CheckboxEmpty and CheckboxFull components

---
+++ web/src/components/icons/checkbox/empty/index.tsx
@@ -0,0 +1,15 @@
+export default function CheckboxEmpty() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width={32} height={32} fill="none">
+      <rect
+        width={28.8}
+        height={28.8}
+        x={1.6}
+        y={1.6}
+        stroke="#fff"
+        strokeWidth={3.2}
+        rx={1.6}
+      />
+    </svg>
+  );
+}
---
+++ web/src/components/icons/checkbox/full/index.tsx
@@ -0,0 +1,20 @@
+export default function CheckboxFull() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width={32} height={32} fill="none">
+      <rect
+        width={28.8}
+        height={28.8}
+        x={1.6}
+        y={1.6}
+        fill="#fff"
+        stroke="#fff"
+        strokeWidth={3.2}
+        rx={1.6}
+      />
+      <path
+        fill="#000"
+        d="m28.565 9.563-16 16a1.5 1.5 0 0 1-2.125 0l-7-7a1.502 1.502 0 1 1 2.125-2.125l5.939 5.938L26.443 7.44a1.503 1.503 0 0 1 2.125 2.125l-.003-.002Z"
+      />
+    </svg>
+  );
+}
